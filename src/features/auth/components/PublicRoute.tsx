2d0e3f63779ffa2a3e24ca8d9e8da7c61309f005
Shubham Thakare <thakare7041@gmail.com>
2026-09-16 19:29:03 +0530

    Logs

--- src/features/auth/components/PublicRoute.tsx
+++ src/features/auth/components/PublicRoute.tsx
@@ -7,11 +7,17 @@ import { useAuth } from "../hooks/useAuth";
 export function PublicRoute() {
   const { isAuthenticated, isLoading } = useAuth();
 
+  console.log("PUBLIC ROUTE:", {
+    isAuthenticated,
+    isLoading,
+  });
+
   if (isLoading) {
     return <LoadingState />;
   }
 
   if (isAuthenticated) {
+    console.log("REDIRECTING TO DASHBOARD");
     return <Navigate to="/dashboard" replace />;
   }
 
